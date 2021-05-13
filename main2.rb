319cea17104ddc9dd247d2efaa5e54f64ba6977d
takurofukuta <mojuraspax@icloud.com>
2021-05-13 22:57:18 +0900

    表示にない商品番号を選んだ際の処理、購入数が0個以下の場合の処理を追加

--- main2.rb
+++ main2.rb
@@ -7,14 +7,23 @@ def disp_products(products)
 end
 
 def choose_product(products)
-  print "商品の番号を選択 >"
-  selected_product_num = gets.to_i
+  while true
+    print "商品の番号を選択 > "
+    selected_product_num = gets.to_i
+    break if (1..products.length).include?(selected_product_num)
+      puts "正しい商品番号を選択してください"
+  end
   chosen_product = products[selected_product_num - 1]
 end
 
 def decide_quantity(chosen_product)
-  print "#{chosen_product[:name]}ですね。何個買いますか？ >"
-  quantity_of_product = gets.to_i
+  while true
+    print "#{chosen_product[:name]}ですね。何個買いますか？ > "
+    quantity_of_product = gets.to_i
+    break if quantity_of_product >= 1
+      puts "一個以上選択してください"
+  end
+  quantity_of_product
 end
 
 def calc_total_price(chosen_product, quantity_of_product)
